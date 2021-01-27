bbbfbd9af4e2a73f81cd5651579d7f9a91796a78
Alexander Sklar <asklar@microsoft.com>
2021-01-27 10:00:56 -0800

    simplify lib for now

--- NET5Lib/Class1.cs
+++ NET5Lib/Class1.cs
@@ -4,5 +4,7 @@ namespace NET5Lib
 {
     public sealed class Class1
     {
+        public string Sauce { get { return "Awesome"; } }
+        public bool IsGreat(string x) { return x == Sauce; }
     }
 }
